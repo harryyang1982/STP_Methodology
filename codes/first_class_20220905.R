20fc9652f84b129f4ef2d22a2b88ef75651af07d
harryyang1982 <harry.seunghoon.yang@gmail.com>
2022-09-05 14:17:03 +0900

    diff

---
+++ codes/first_class_20220905.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+2 + 2
+
+2 * 4
+2 ** 4
+2 ^ 4
+
+(1 + 3 + 2 + 12 + 8) / 5
+
+2 ^^ 2
+2 ** 2
+
+x <- 2
+
+mtcars %>% 
+  filter(mpg >= 20) -> mtcars_high
+
+
+x <- "two"
+
+typeof(x)
+str(x)
+
